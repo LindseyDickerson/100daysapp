f603e3f395ae4e88a47f74c7fb0572ca17cad852
Lindsey Dickerson <lindseydickerson2@gmail.com>
2020-01-16 19:26:13 -0500

    first commit

---
+++ src/components/site/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Row } from "reactstrap";
+
+const Footer = () => {
+    return (
+        <footer>
+            <Row>
+                <p>&copy; Lindsey Dickerson 2020</p>
+            </Row>
+        </footer>
+    );
+};
+
+export default Footer;
